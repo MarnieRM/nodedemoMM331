bf5d231cd6e0e466e30b898da93efc415e138187
MarnieRM <56393868+MarnieRM@users.noreply.github.com>
2022-10-24 16:27:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 
 http.createServer(function(request, response) {    
    response.writeHead(200, {"Content-Type": "text/html"});
-   response.write("<h1>Hello, Node.js!</h1>");    
+   response.write("<h1>Hello, Node.js!</h1>");
+   response.write("") //Add stuff here?
    response.end();
 }).listen(3008);
